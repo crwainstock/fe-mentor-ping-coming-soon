bae1b19b9584a139d2af3af98f0d55ba2930dca6
Crystal <crwainstock@gmail.com>
2023-11-05 13:15:54 +0100

    update js to include userEmailInput

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const button = document.querySelector("button");
+const userEmailInput = document.getElementById("email");
+
 button.addEventListener("click", submitEmail);
 
 let userEmail = document.getElementById("email");
 
 const submitEmail = (event) => {
   event.preventDefault();
+
+  // Get the value of the email input
+  const userEmail = userEmailInput.value;
 
   if (userEmail) {
     // If the userEmail is an email address
