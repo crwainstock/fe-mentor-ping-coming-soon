cfb105f066f1f563f061b85722962a6670e29413
Crystal <crwainstock@gmail.com>
2023-11-05 13:21:09 +0100

    add isValidEmail function to js

--- index.js
+++ index.js
@@ -11,11 +11,32 @@ const submitEmail = (event) => {
   // Get the value of the email input
   const userEmail = userEmailInput.value;
 
-  if (userEmail) {
+  if (isValidEmail(userEmail)) {
     // If the userEmail is an email address
     // submit the email address
     // show success message below input
+
+    showSuccessMessage();
   } else {
     // Turn input red, add error message below input
+    userEmailInput.style.borderColor = "hsl(354, 100%, 66%)";
+    showErrorMessage("Please enter a valid email address.");
   }
 };
+
+function isValidEmail(email) {
+  // Check the format of the input -- standard characters @ standard characters. ending
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+function showSuccessMessage() {
+  // Implement your logic to show a success message below the input field.
+  // For example, you can create a div element and add it to the DOM.
+  // Make sure to clear any previous error styles/messages if needed.
+}
+
+function showErrorMessage(message) {
+  // Implement your logic to show an error message below the input field.
+  // For example, you can create a div element, set the error message, and add it to the DOM.
+}
